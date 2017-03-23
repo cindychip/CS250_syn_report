3220887fe215318971f371675ec22c06962f970c
cindychip <cindy.chen0316@gmail.com>
2017-03-22 19:52:57 -0700

    clean up the code

--- src/main/scala/dfe3/filter_template_complex_v2.scala
+++ src/main/scala/dfe3/filter_template_complex_v2.scala
@@ -24,18 +24,20 @@ class fir[T <: Data:RealBits](gen: => T,var window_size: Int) extends Module {
   val io = IO(new fir_Io(gen))
 
   val delays = Reg(Vec(window_size, DspComplex(gen, gen)))
-  delays(0) := io.input_complex
- 
-  for (i<- 1 until window_size) {
-  	delays(i) := delays(i-1)
-  }
   val coeff_count = Reg(init = 0.U(16.W)) //max 512 in darpa
   val buffer_complex = Reg(Vec(window_size, DspComplex(gen, gen))) //vector of reg
   val sum = Wire(Vec(window_size, DspComplex(gen,gen)))
+  //shift register
+  delays(0) := io.input_complex
+  for (i<- 1 until window_size) {
+  	delays(i) := delays(i-1)
+  }
+  //coeff update
   when (coeff_count < window_size.asUInt) {
   	buffer_complex(coeff_count) := io.tap_coeff_complex
   	coeff_count := coeff_count + 1.U
   }
+  //sum up and generate output
   sum(0) := delays(0) * buffer_complex(0)
   for (i <- 1 until window_size) {
   	sum(i) := delays(i) * buffer_complex(i) + sum(i-1)
