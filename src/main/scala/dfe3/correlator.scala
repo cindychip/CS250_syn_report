444fc6cf0f280d17b7373dc859aa287ad72203c4
Kate Du <kaidi_du@berkeley.edu>
2017-03-22 23:07:03 -0700

    Update correlator

--- src/main/scala/dfe3/correlator.scala
+++ src/main/scala/dfe3/correlator.scala
@@ -17,39 +17,38 @@ import dsptools.numbers._
 class correlatorIo[T <: Data:RealBits](gen: T) extends Bundle {
   val input_complex = Input(DspComplex(gen.cloneType, gen.cloneType))
   val output_complex = Output(DspComplex(gen.cloneType, gen.cloneType))
- // val preamble = Input(DspComplex(gen.cloneType, gen.cloneType))
   val fbf_coeff = Output(DspComplex(gen.cloneType, gen.cloneType))
   override def cloneType: this.type = new correlatorIo(gen).asInstanceOf[this.type]
 }
-class correlator[T <: Data:RealBits](gen: T, val n: Int) extends Module {
+
+class correlator[T <: Data:RealBits](gen: T, var n: Int ,var preambleRe: Array[Double], var preambleIm: Array[Double]) extends Module {
+// Calculate preamble reference j
   val io = IO(new correlatorIo(gen))
-  //state machine initialization
-   val endSignal = Reg(init = 0.U) // for testing shift two signals
+  val preambleReFix = preambleRe.map(tap => ConvertableTo[T].fromDouble(tap))
+  val preambleImFix = preambleIm.map ( tap =>ConvertableTo[T].fromDouble(tap))
+  val preambleConjImFix = preambleImFix.map(tap => -tap)
+  val RefRe1 = preambleReFix.map{tap: T => tap*tap}
+  val RefRe2 = preambleImFix.map{tap: T => tap*tap}
+  val RefIm1 = preambleReFix.zip(preambleImFix).map{case(x,y) => -x*y }
+  val RefIm2 = preambleImFix.zip(preambleReFix).map{case(x,y) => x*y }
+  val RefRe = RefRe1.zip(RefRe2).map{case(x,y) => x+y}.reduceLeft( _ + _ )
+  val RefIm = RefIm1.zip(RefIm2).map{case(x,y) => x+y}.reduceLeft( _ + _ )
+  val preambleConjFix = preambleReFix.zip(preambleConjImFix).map{case(x,y) => DspComplex(x, y)}
+  val Ref = DspComplex(RefRe, RefIm)
+
+//state machine initialization
+   val endSignal = RegInit(init = 0.U) // for testing shift two signals
    val initial :: calculate :: shift :: Nil = Enum(3)
    val shiftState = Reg(init=initial)
    val counter = Reg(init = 0.U)
    //ShiftRegister initialization
-   val preamble = Reg(Vec(n, DspComplex(gen, gen)))
-   val preambleEn = Reg(init = true.B)
    val delays = Reg(Vec(n, DspComplex(gen, gen)))
-   val Multi   = Array.fill(n)(Module(new Multiply(gen)).io)
+  // val Multi   = Array.fill(n)(Module(new Multiply(gen)).io)
    val sum  = Wire(Vec(n, DspComplex(gen, gen)))
-   val init = Reg(init = true.B)
-   val ref = Reg(DspComplex(gen, gen))
    // Set up ShiftRegister
    delays(0) := io.input_complex
    for (i<- 1 until n) {
           delays(i) := delays(i-1)
-    }
-    when(preambleEn){
-    	preamble(0):=io.input_complex
-        for (i<- 1 until n) {
-          preamble(i) := preamble(i-1)
-        }
-    }.otherwise{
-    	for (i<- 0 until n) {
-          preamble(i) := preamble(i)
-        }
     }
 
    switch(shiftState) {
@@ -57,24 +56,15 @@ class correlator[T <: Data:RealBits](gen: T, val n: Int) extends Module {
      	when (counter < n){  
           counter := counter + 1.U
         }.otherwise{
-          preambleEn := false.B
+         // preambleEn := false.B
           counter := 0.U
           shiftState := calculate
         }
       }
      is(calculate){
-        Multi(0).cx := preamble(0).conj()
-        Multi(0).cy := delays(0)
-        sum(0) := Multi(0).out
+        sum(0) := DspContext.withComplexUse4Muls(true) { delays(0) * preambleConjFix(0) }
         for (i <- 1 until n) {
-          Multi(i).cx := preamble(i).conj()
-          Multi(i).cy := delays(i)
-          sum(i) := Multi(i).out+sum(i-1)
-        }
-        when (init){
-        	ref:= sum(n-1).conj()
-        	//ref:= sum(n-1).conj().div2(DspContext.withComplexUse4Muls(true) { sum(n-1)*sum(n-1).conj()})
-        	init := false.B
+         sum(i) := sum(i-1) + DspContext.withComplexUse4Muls(true) { delays(i) * preambleConjFix(i) }
         }
         io.fbf_coeff := sum(n-1) //divide by ref
         io.output_complex := delays(n-1)
@@ -95,6 +85,7 @@ class correlator[T <: Data:RealBits](gen: T, val n: Int) extends Module {
       }
     }
 }
+
 
 
 
--- src/test/scala/dfe3/correlator_test.scala
+++ src/test/scala/dfe3/correlator_test.scala
@@ -16,17 +16,22 @@ import breeze.signal._
 class correlatorTests[T <: Data:RealBits](c: correlator[T]) extends DspTester(c) {
  
 
-  var len = 4
-  val real = Array(1.0, 2.0)
-  val img = Array(1.0, 2.0)
+  val len = 4
+  val real = Array(1.0, 2.0, 3.0, 4.0)
+  val img = Array(1.0, 2.0, 3.0, 4.0)
+  val preamble_real = Array(1.0, 2.0)
+  val preamble_img = Array(1.0, 2.0)
   //val real = Array.fill(len)(Random.nextDouble*2-1)
   //val img = Array.fill(len)(Random.nextDouble*2-1)
   val size = 2
+  //val expect_real = new Array[Double](len-size+1)
+  //val expect_real = new Array[Double](len-size+1)
+  val (expect_real, expect_img) = Adder(real, img, preamble_real, preamble_img,
+                                     len, size)
   for (i<-0 until size){
-    val (expect_real,expect_img) = Adder(real, img, real, img,
-                                     size, size)
-    println("expect_real --> " + expect_real)
-    //  poke (c.io.input_complex.real,real(i))
+    
+    println("expect_real --> " + expect_real(i))
+  //  poke (c.io.input_complex.real,real(i))
     //  poke (c.io.input_complex.imag, img(i))
     //  step(1)
     //  expect (c.io.output_complex.real, expect_real)
@@ -36,6 +41,9 @@ class correlatorTests[T <: Data:RealBits](c: correlator[T]) extends DspTester(c)
 
 // Scala style testing
 class correlatorSpec extends FlatSpec with Matchers {
+  val real = Array(1.0, 2.0, 3.0, 4.0)
+  val img = Array(1.0, 2.0, 3.0, 4.0)
+
   val testOptions = new DspTesterOptionsManager {
     dspTesterOptions = DspTesterOptions(
         fixTolLSBs = 1,
@@ -50,7 +58,7 @@ class correlatorSpec extends FlatSpec with Matchers {
   behavior of "correlator module"
 
   it should "properly add fixed point types" in {
-dsptools.Driver.execute(() => new correlator(FixedPoint(32.W, 12.BP),2), testOptions) { c =>      
+dsptools.Driver.execute(() => new correlator(FixedPoint(32.W, 12.BP),4, real, img), testOptions) { c =>      
   new correlatorTests(c)
     } should be (true)
   }
@@ -59,27 +67,48 @@ dsptools.Driver.execute(() => new correlator(FixedPoint(32.W, 12.BP),2), testOpt
 object Adder {
   def apply(signal_real:Array[Double], signal_img: Array[Double], 
             preamble_real:Array[Double], preamble_img:Array[Double], 
-            sig_len:Int , coef_len:Int) : (Double, Double)={
-   val out_real = new Array[Double](sig_len+coef_len)
-   val out_img = new Array[Double](sig_len+coef_len)
-   for (i <- 0 until coef_len) {
-      val tmp_real1 = signal_real.map(_*preamble_real(i)) 
-      val tmp_real2 = signal_img.map(_*preamble_img(i))
-      val tmp_img1 = signal_img.map(_*preamble_real(i)) 
-      val tmp_img2 = signal_real.map(_*preamble_img(i))
-      //NOT SURE:
-      for (j <- i until sig_len+i) {
-        out_real(j) = out_real(j) + tmp_real1(j-i) - tmp_real2(j-i)
-        out_img(j) = out_img(j) + tmp_img1(j-i) + tmp_img2(j-i)
+            sig_len:Int , preamble_len:Int) : (Array[Double], Array[Double])={
+   val tmp_Re = new Array[Double](preamble_len)
+   val tmp_Im = new Array[Double](preamble_len)
+   val outRe = new Array[Double](sig_len-preamble_len+1)
+   val outIm = new Array[Double](sig_len-preamble_len+1)
+   for(j<-0 until (sig_len-preamble_len+1)){
+      for (i<-0 until preamble_len){
+      tmp_Re(i) = preamble_real(i)*signal_real(i+j)+preamble_img(i)*signal_img(i+j)
+      tmp_Im(i) = preamble_real(i)*preamble_img(i)-preamble_img(i)*preamble_img(i)
       }
-    }//end for loop
-    // for (j <- 0 until sig_len) {
-    //     out_real(j) = tmp_real1(j) + tmp_real2(j)
-    //     out_img(j) = tmp_img1(j) + tmp_img2(j)
-    // }
-    val real = out_real.sum
-    val img = out_img.sum
-    return (real,img)
-  } 
+      outRe(j) = tmp_Re.reduceLeft( _ + _ )
+      outIm(j) = tmp_Im.reduceLeft( _ + _ )
+    }
+   return (outRe, outIm)
+   }
+ }
 
-}
+
+
+
+
+
+//    val out_real = new Array[Double](sig_len+coef_len)
+//    val out_img = new Array[Double](sig_len+coef_len)
+//    for (i <- 0 until coef_len) {
+//       val tmp_real1 = signal_real.map(_*preamble_real(i)) 
+//       val tmp_real2 = signal_img.map(_*preamble_img(i))
+//       val tmp_img1 = signal_img.map(_*preamble_real(i)) 
+//       val tmp_img2 = signal_real.map(_*preamble_img(i))
+//       //NOT SURE:
+//       for (j <- i until sig_len+i) {
+//         out_real(j) = out_real(j) + tmp_real1(j-i) - tmp_real2(j-i)
+//         out_img(j) = out_img(j) + tmp_img1(j-i) + tmp_img2(j-i)
+//       }
+//     }//end for loop
+//     // for (j <- 0 until sig_len) {
+//     //     out_real(j) = tmp_real1(j) + tmp_real2(j)
+//     //     out_img(j) = tmp_img1(j) + tmp_img2(j)
+//     // }
+//     val real = out_real.sum
+//     val img = out_img.sum
+//     return (real,img)
+//   } 
+
+// }
