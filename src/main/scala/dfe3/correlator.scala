cbdb96fef9b96d92e01fa7c8cecd3881f3f8e825
Kate Du <kaidi_du@berkeley.edu>
2017-04-03 12:40:23 -0700

    Golay Correlator Update

--- src/main/scala/dfe3/correlator.scala
+++ src/main/scala/dfe3/correlator.scala
@@ -17,32 +17,108 @@ import dsptools.numbers._
 class correlatorIo[T <: Data:RealBits](gen: T) extends Bundle {
   val input_complex = Input(DspComplex(gen.cloneType, gen.cloneType))
   val output_complex = Output(DspComplex(gen.cloneType, gen.cloneType))
-  val fbf_coeff = Output(DspComplex(gen.cloneType, gen.cloneType))
+  val ra_out = Output(DspComplex(gen.cloneType, gen.cloneType))
+  val rb_out = Output(DspComplex(gen.cloneType, gen.cloneType))
   override def cloneType: this.type = new correlatorIo(gen).asInstanceOf[this.type]
 }
 
-class correlator[T <: Data:RealBits](gen: T, var n: Int ,var preambleRe: Array[Double], var preambleIm: Array[Double]) extends Module {
-// Calculate preamble reference j
-  val io = IO(new correlatorIo(gen))
-  val preambleReFix = preambleRe.map(tap => ConvertableTo[T].fromDouble(tap))
-  val preambleImFix = preambleIm.map ( tap =>ConvertableTo[T].fromDouble(tap))
-  //ShiftRegister initialization
-  val delays = Reg(Vec(n, DspComplex(gen, gen)))
-  val sum  = Wire(Vec(n, DspComplex(gen, gen)))
-  // Set up ShiftRegister
-  delays(0) := io.input_complex
-  for (i<- 1 until n) {
-  	delays(i) := delays(i-1)
+class correlator[T <: Data:RealBits](gen: T) extends Module {
+val io = IO(new correlatorIo(gen))
+//Set up constant 
+val n = 7
+val W = List(-1, -1, -1, -1, 1, -1, -1)
+val Dk = List(1, 8, 2, 4, 16, 32, 64)
+val D1 = Reg(Vec(Dk(0), DspComplex(gen, gen)))
+val D2 = Reg(Vec(Dk(1), DspComplex(gen, gen)))
+val D3 = Reg(Vec(Dk(2), DspComplex(gen, gen)))
+val D4 = Reg(Vec(Dk(3), DspComplex(gen, gen)))
+val D5 = Reg(Vec(Dk(4), DspComplex(gen, gen)))
+val D6 = Reg(Vec(Dk(5), DspComplex(gen, gen)))
+val D7 = Reg(Vec(Dk(6), DspComplex(gen, gen)))
+val DW = Wire(Vec(n, DspComplex(gen, gen)))
+val ra  = Wire(Vec(n, DspComplex(gen, gen)))
+val rb  = Wire(Vec(n, DspComplex(gen, gen)))
+
+// Set up ShiftRegister
+//D1
+D1(0) := io.input_complex
+DW(0) := D1(0)
+//D2
+for (i<- 0 until Dk(1)) {
+  if(i == 0){
+    D2(0) := rb(0) /////////////////////
+  }else{
+    D2(i) := D2(i-1)
   }
-  sum(0).real := delays(0).real * preambleReFix(n-1) + delays(0).imag * preambleImFix(n-1)
-  sum(0).imag := delays(0).imag * preambleReFix(n-1) - delays(0).real * preambleImFix(n-1)
-  for (i <- 1 until n) {
-  	sum(i).real := sum(i-1).real + delays(i).real * preambleReFix(n-i-1) + delays(i).imag * preambleImFix(n-i-1)
-	sum(i).imag := sum(i-1).imag + delays(i).imag * preambleReFix(n-i-1) - delays(i).real * preambleImFix(n-i-1)
-    }
-  io.fbf_coeff := sum(n-1) 	 
-  io.output_complex := delays(n-1)
 }
+DW(1) := D2(Dk(1)-1)
+//D3
+for (i<- 0 until Dk(2)) {
+  if(i == 0){
+    D3(0) := rb(1)///////////////////////////
+  }else{
+    D3(i) := D3(i-1)
+  }
+}
+DW(2) := D3(Dk(2)-1)
+//D4
+for (i<- 0 until Dk(3)) {
+  if(i == 0){
+    D4(0) := rb(2) ///////////////////
+  }else{
+    D4(i) := D4(i-1)
+  }
+}
+DW(3) := D4(Dk(3)-1)
+//D5
+for (i<- 0 until Dk(3)) {
+  if(i == 0){
+    D5(0) := rb(3)///////////////////////
+  }else{
+    D5(i) := D5(i-1)
+  }
+}
+DW(4) := D5(Dk(4)-1)
+//D6
+for (i<- 0 until Dk(3)) {
+  if(i == 0){
+    D6(0) := rb(4)////////////
+  }else{
+    D6(i) := D6(i-1)
+  }
+}
+DW(5) := D6(Dk(5)-1)
+//D7
+for (i<- 0 until Dk(3)) {
+  if(i == 0){
+    D7(0) := rb(5)///////////////
+  }else{
+    D7(i) := D7(i-1)
+  }
+}
+DW(6) := D7(Dk(6)-1)
+// Calculate ra
+for (i <- 0 until n){
+  if (i == 0){
+    ra(i) := -io.input_complex+DW(i)
+  }else if (i == 4){
+    ra(i) := ra(i-1)+DW(i)
+  }else{
+    ra(i) := -ra(i-1)+DW(i)
+  }
+}
+// Calculate rb
+for (i <- 0 until n){
+  if (i == 0){
+    rb(i) := -io.input_complex-DW(i)
+  }else if (i == 4){
+    rb(i) := rb(i-1)-DW(i)
+  }else{
+    rb(i) := -rb(i-1)-DW(i)
+  }
+}
+io.ra_out := ra(6) 
+io.rb_out := rb(6)
 
 
 
@@ -50,3 +126,27 @@ class correlator[T <: Data:RealBits](gen: T, var n: Int ,var preambleRe: Array[D
 
 
 
+
+
+
+// // Calculate preamble reference j
+//   val io = IO(new correlatorIo(gen))
+//   val preambleReFix = preambleRe.map(tap => ConvertableTo[T].fromDouble(tap))
+//   val preambleImFix = preambleIm.map ( tap =>ConvertableTo[T].fromDouble(tap))
+//   //ShiftRegister initialization
+//   val delays = Reg(Vec(n, DspComplex(gen, gen)))
+//   val sum  = Wire(Vec(n, DspComplex(gen, gen)))
+//   // Set up ShiftRegister
+//   delays(0) := io.input_complex
+//   for (i<- 1 until n) {
+//   	delays(i) := delays(i-1)
+//   }
+//   sum(0).real := delays(0).real * preambleReFix(n-1) + delays(0).imag * preambleImFix(n-1)
+//   sum(0).imag := delays(0).imag * preambleReFix(n-1) - delays(0).real * preambleImFix(n-1)
+//   for (i <- 1 until n) {
+//   	sum(i).real := sum(i-1).real + delays(i).real * preambleReFix(n-i-1) + delays(i).imag * preambleImFix(n-i-1)
+// 	sum(i).imag := sum(i-1).imag + delays(i).imag * preambleReFix(n-i-1) - delays(i).real * preambleImFix(n-i-1)
+//     }
+//   io.fbf_coeff := sum(n-1) 	 
+//   io.output_complex := delays(n-1)
+}
--- src/test/scala/dfe3/correlator_test.scala
+++ src/test/scala/dfe3/correlator_test.scala
@@ -14,32 +14,43 @@ import breeze.math.Complex
 import breeze.signal._
 
 class correlatorTests[T <: Data:RealBits](c: correlator[T]) extends DspTester(c) {
-  val len = 4
-  val size = 2
-  val real = Array.fill(len)(Random.nextDouble*2-1)
-  val img = Array.fill(len)(Random.nextDouble*2-1)
-  val preamble_real = Array(11.0, 12.0)
-  val preamble_img = Array(9.0, 10.0)
-  val (expect_real, expect_img) = Adder(real, img, preamble_real, preamble_img,
-                                     len, size)
-  for (i<-0 until len){
-    poke (c.io.input_complex.real,real(i))
-    poke (c.io.input_complex.imag, img(i))
-    step(1)
-    if (i>=size-1){
-      expect (c.io.fbf_coeff.real, expect_real(i-size+1))
-      expect (c.io.fbf_coeff.imag, expect_img(i-size+1))
-      expect (c.io.output_complex.real, real(i-size+1))
-      expect (c.io.output_complex.imag, img(i-size+1))
+val ga128 = Array(+1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 +1 +1 -1 -1 +1 +1 +1 +1 -1 +1 -1 +1 -1 +1 +1 -1 -1 -1 +1 +1 +1 +1 +1 +1 +1 -1 +1 -1 -1 +1 +1 -1 +1 +1 -1 -1 +1 +1 +1 +1 -1 +1 -1 +1 -1 +1 +1 -1 +1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 +1 +1 -1 -1 +1 +1 +1 +1 -1 +1 -1 +1 -1 +1 +1 -1 +1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 -1 -1 +1 +1 -1 -1 -1 -1 +1 -1 +1 -1 +1 -1 -1 +1)
+val ga128 = Array(-1 -1 +1 +1 +1 +1 +1 +1 +1 -1 +1 -1 -1 +1 +1 -1 -1 -1 +1 +1 -1 -1 -1 -1 +1 -1 +1 -1 +1 -1 -1 +1 +1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 -1 -1 +1 +1 -1 -1 -1 -1 +1 -1 +1 -1 +1 -1 -1 +1 +1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 +1 +1 -1 -1 +1 +1 +1 +1 -1 +1 -1 +1 -1 +1 +1 -1 +1 +1 -1 -1 -1 -1 -1 -1 -1 +1 -1 +1 +1 -1 -1 +1 -1 -1 +1 +1 -1 -1 -1 -1 +1 -1 +1 -1 +1 -1 -1 +1)
+val n = 128
 
-    }
-  }//end for
+for (i<-0 until n){
+  poke (c.io.input_complex.real,ga128())
+  poke (c.io.input_complex.imag, 0)
+  step(1)
+}
+println("Real -----------> " + peek(c.io.ra_out.real))
+
+
+
+  // val len = 4
+  // val size = 2
+  // val real = Array.fill(len)(Random.nextDouble*2-1)
+  // val img = Array.fill(len)(Random.nextDouble*2-1)
+  // val preamble_real = Array(11.0, 12.0)
+  // val preamble_img = Array(9.0, 10.0)
+  // val (expect_real, expect_img) = Adder(real, img, preamble_real, preamble_img,
+  //                                    len, size)
+  // for (i<-0 until len){
+  //   poke (c.io.input_complex.real,real(i))
+  //   poke (c.io.input_complex.imag, img(i))
+  //   step(1)
+  //   if (i>=size-1){
+  //     expect (c.io.fbf_coeff.real, expect_real(i-size+1))
+  //     expect (c.io.fbf_coeff.imag, expect_img(i-size+1))
+  //     expect (c.io.output_complex.real, real(i-size+1))
+  //     expect (c.io.output_complex.imag, img(i-size+1))
+
+  //   }
+  // }//end for
 }
 
 // Scala style testing
 class correlatorSpec extends FlatSpec with Matchers {
-  val preamble_real = Array(11.0, 12.0)
-  val preamble_img = Array(9.0, 10.0)
 
   val testOptions = new DspTesterOptionsManager {
     dspTesterOptions = DspTesterOptions(
@@ -55,28 +66,28 @@ class correlatorSpec extends FlatSpec with Matchers {
   behavior of "correlator module"
 
   it should "properly add fixed point types" in {
-dsptools.Driver.execute(() => new correlator(FixedPoint(32.W, 12.BP),2, preamble_real, preamble_img), testOptions) { c =>      
+dsptools.Driver.execute(() => new correlator(FixedPoint(32.W, 12.BP)), testOptions) { c =>      
   new correlatorTests(c)
     } should be (true)
   }
 }
 
-object Adder {
-  def apply(signal_real:Array[Double], signal_img: Array[Double], 
-            preamble_real:Array[Double], preamble_img:Array[Double], 
-            sig_len:Int , preamble_len:Int) : (Array[Double], Array[Double])={
-   val tmp_Re = new Array[Double](preamble_len)
-   val tmp_Im = new Array[Double](preamble_len)
-   val outRe = new Array[Double](sig_len-preamble_len+1)
-   val outIm = new Array[Double](sig_len-preamble_len+1)
-   for(j<-0 until (sig_len-preamble_len+1)){
-      for (i<-0 until preamble_len){
-      tmp_Re(i) = preamble_real(i)*signal_real(i+j)-(-preamble_img(i))*signal_img(i+j)
-      tmp_Im(i) = preamble_real(i)*signal_img(i+j)+(-preamble_img(i))*signal_real(i+j)
-      }
-      outRe(j) = tmp_Re.reduceLeft( _ + _ )
-      outIm(j) = tmp_Im.reduceLeft( _ + _ )
-    }
-   return (outRe, outIm)
-   }
- }
+// object Adder {
+//   def apply(signal_real:Array[Double], signal_img: Array[Double], 
+//             preamble_real:Array[Double], preamble_img:Array[Double], 
+//             sig_len:Int , preamble_len:Int) : (Array[Double], Array[Double])={
+//    val tmp_Re = new Array[Double](preamble_len)
+//    val tmp_Im = new Array[Double](preamble_len)
+//    val outRe = new Array[Double](sig_len-preamble_len+1)
+//    val outIm = new Array[Double](sig_len-preamble_len+1)
+//    for(j<-0 until (sig_len-preamble_len+1)){
+//       for (i<-0 until preamble_len){
+//       tmp_Re(i) = preamble_real(i)*signal_real(i+j)-(-preamble_img(i))*signal_img(i+j)
+//       tmp_Im(i) = preamble_real(i)*signal_img(i+j)+(-preamble_img(i))*signal_real(i+j)
+//       }
+//       outRe(j) = tmp_Re.reduceLeft( _ + _ )
+//       outIm(j) = tmp_Im.reduceLeft( _ + _ )
+//     }
+//    return (outRe, outIm)
+//    }
+//  }
