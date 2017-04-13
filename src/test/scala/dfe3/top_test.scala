34f0a2405d4dfae7b5142f5c304d58aa58408f38
cindychip <cindy.chen0316@gmail.com>
2017-04-13 12:26:34 -0700

    top module test

---
+++ src/test/scala/dfe3/top_test.scala
@@ -0,0 +1,55 @@
+package dfe3
+import chisel3._
+import scala.util.Random
+import chisel3.experimental.FixedPoint
+import dsptools.numbers.{RealBits}
+import dsptools.numbers.implicits._
+import dsptools.DspContext
+import dsptools.{DspTester, DspTesterOptionsManager, DspTesterOptions}
+import iotesters.TesterOptions
+import org.scalatest.{FlatSpec, Matchers}
+import math._
+import dsptools.numbers._
+import breeze.math.Complex
+import breeze.signal._
+import scala.io.Source  // Added
+
+class dfeTests[T <: Data:RealBits](c: dfe3[T]) extends DspTester(c) {
+//val ga128 = Array(1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1)
+//val gab128 = Array(1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1,-1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1)
+//val gb128 = Array(-1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, 1)
+val real = Source.fromFile("/scratch/cs250-aac/dfe/src/test/scala/dfe3/filter_real.txt").getLines.toArray.map(x => x.toDouble)
+val imag = Source.fromFile("/scratch/cs250-aac/dfe/src/test/scala/dfe3/filter_imag.txt").getLines.toArray.map(x => x.toDouble)
+val n = real.length
+for (i<-0 until n){
+    poke (c.io.signal_in.real,real(i))
+    poke (c.io.signal_in.imag, imag(i))
+    poke (c.io.enable, true)
+    poke (c.io.reset, false)
+    peek (c.io.signal_out)
+    step(1)
+  }
+}
+
+// Scala style testing
+class dfeSpec extends FlatSpec with Matchers {
+
+  val testOptions = new DspTesterOptionsManager {
+    dspTesterOptions = DspTesterOptions(
+        fixTolLSBs = 1,
+        genVerilogTb = true,
+        isVerbose = true)
+    testerOptions = TesterOptions(
+        isVerbose = false,
+        backendName = "verilator")
+    commonOptions = commonOptions.copy(targetDirName = "test_run_dir/correlator_fix")
+  }
+
+  behavior of "correlator module"
+
+  it should "properly add fixed point types" in {
+dsptools.Driver.execute(() => new dfe3(FixedPoint(32.W, 12.BP)), testOptions) { c =>      
+  new dfeTests(c)
+    } should be (true)
+  }
+}
