64483e1f43c932a5a08dfc802092df8cca8f2ed7
cindychip <cindy.chen0316@gmail.com>
2017-04-22 15:20:39 -0700

    Update top_test.scala

--- src/test/scala/dfe3/top_test.scala
+++ src/test/scala/dfe3/top_test.scala
@@ -42,8 +42,8 @@ for (i<-0 until 300){
 
 
     if(i > 255) { ///correct
-      expect(c.io.signal_out.real,test_real(i-255))
-      expect(c.io.signal_out.imag,test_imag(i-255))
+      expect(c.io.signal_out.real,test_real(i-254))
+      expect(c.io.signal_out.imag,test_imag(i-254))
     }
     step(1)
 
