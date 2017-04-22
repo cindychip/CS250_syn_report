ec4bc24e3b16f660dd042d95264206c0ba6956de
cindychip <cindy.chen0316@gmail.com>
2017-04-22 12:53:11 -0700

    Update top_test.scala

--- src/test/scala/dfe3/top_test.scala
+++ src/test/scala/dfe3/top_test.scala
@@ -73,6 +73,8 @@ dsptools.Driver.execute(() => new dfe3Main(FixedPoint(22, 12)), testOptions) { c
   }
 }
 
+class DefaultConfig() extends Config {
+}
 
 object dfeTester extends App {
   //We pass in some positional arguments to make things easier
